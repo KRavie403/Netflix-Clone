b938c105772b9a13e644706b9ff7562a735b115d
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-04 21:03:59 +0900

    feat: Router설정하여 페이지 연결
    Home, Popular, Search, Wishlist, SignIn, SignUp, MovieDetail 페이지에 대한 라우트 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Popular from './components/Popular';
+import Search from './components/Search';
+import Wishlist from './components/Wishlist';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/popular" component={Popular} />
+        <Route path="/search" component={Search} />
+        <Route path="/wishlist" component={Wishlist} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
