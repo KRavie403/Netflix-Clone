52482f07748df0f0284d9712bc6ad832c3e561c1
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-19 18:47:56 +0900

    updated: 주석처리

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Switch -> Routes로 변경
 import Header from './components/Header';
 import Home from './components/Home';
-import Popular from './components/Popular';
+import Popular from './components/Popular/popular';
 import Search from './components/Search';
 import Wishlist from './components/Wishlist';
-import SignIn from './components/SignIn.js';
-import SignUp from './components/SignUp.js/index.js';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes> {/* Switch -> Routes로 변경 */}
-        <Route path="/" element={<Home />} /> {/* component 대신 element 사용 */}
+        {/* {<Route path="/" element={<Home />} />} */}
         <Route path="/popular" element={<Popular />} />
-        <Route path="/search" element={<Search />} />
+        {/* <Route path="/search" element={<Search />} />
         <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} /> */}
+        {/* <Route path="/signup" element={<SignUp />} /> */}
       </Routes>
     </Router>
   );
