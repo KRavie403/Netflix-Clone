341c008e652068919a5e8ba1e533318f3c953465
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-25 22:54:52 +0900

    updated: add routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './pages/Header';
 import Home from './pages/Home';
 import Popular from './pages/Popular';
-// import Search from './pages/Search';
-// import Wishlist from './pages/Wishlist';
-// import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import Search from './pages/Search';
+import Wishlist from './pages/Wishlist';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/popular" element={<Popular />} />
-        S{/* <Route path="/search" element={<Search />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/signin" element={<SignIn />} /> */}
-        {/* <Route path="/signup" element={<SignUp />} /> */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
