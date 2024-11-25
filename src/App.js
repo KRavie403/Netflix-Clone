eb5d23d250084b74b76387b76c2f452df7b1b492
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-25 18:58:19 +0900

    updated: App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Switch -> Routes로 변경
 import Header from './components/Header';
 import Home from './components/Home';
-import Popular from './components/Popular/popular';
+import Popular from './components/Popular';
 import Search from './components/Search';
 import Wishlist from './components/Wishlist';
 import SignIn from './components/SignIn';
