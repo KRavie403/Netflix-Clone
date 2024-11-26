84117e86dbf79957c0b324c3a105047dc8875895
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-26 17:54:08 +0900

    updated: header에 navbar 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <>
-      <Navbar />
-      <div style={{ paddingTop: "60px" }}>
         <h1>home</h1>
-      </div>
     </>
   );
 }
