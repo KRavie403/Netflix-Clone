03a6919a96cc9ae674568ef860cab6f87b2fdd3d
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-26 22:06:02 +0900

    updated: 로그인-회원가입 공통

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './pages/Header';
 import Home from './pages/Home';
 import Popular from './pages/Popular';
 import Search from './pages/Search';
 import Wishlist from './pages/Wishlist';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태 관리
+
   return (
     <Router>
-      <Header />
+      {/* 로그인 상태에 따라 Header 렌더링 */}
+      {isLoggedIn && <Header />}
+      
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/popular" element={<Popular />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        {/* 로그인 경로 */}
+        <Route path="/signin" element={<SignIn onLogin={() => setIsLoggedIn(true)} />} />
+
+        {/* 로그인되지 않았으면 /signin으로 리디렉션 */}
+        <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/signin" />} />
+        <Route path="/popular" element={isLoggedIn ? <Popular /> : <Navigate to="/signin" />} />
+        <Route path="/search" element={isLoggedIn ? <Search /> : <Navigate to="/signin" />} />
+        <Route path="/wishlist" element={isLoggedIn ? <Wishlist /> : <Navigate to="/signin" />} />
       </Routes>
     </Router>
   );
